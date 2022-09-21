2a4d454e83850f1a21906e3fadc8d4b37aa287fc
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2022-09-20 22:29:27 -0500

    feat: Upgrae area of Tecnology Skills

---
+++ src/portafolio/components/technology-skills/SliderSkills.jsx
@@ -0,0 +1,90 @@
+import { Box, Slider, Tooltip, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const ValueLabelComponent = (props) => {
+  const { children, value } = props;
+
+  return (
+    <Tooltip enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const marks = [
+  { value: 0 },
+  { value: 20 },
+  { value: 37 },
+  { value: 100 },
+];
+
+const IOSSlider = styled(Slider)(({ theme }) => ({
+  color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff !important',
+  height: 2,
+  padding: '15px 0',
+  '& .MuiSlider-thumb': {
+    height: 28,
+    width: 28,
+    backgroundColor: '#fff',
+    boxShadow: iOSBoxShadow,
+    '&:focus, &:hover, &.Mui-active': {
+      boxShadow:
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    fontSize: 12,
+    fontWeight: 'normal',
+    top: -6,
+    backgroundColor: 'unset',
+    color: theme.palette.text.primary,
+    '&:before': {
+      display: 'none',
+    },
+    '& *': {
+      background: 'transparent',
+      color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+    },
+  },
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-rail': {
+    opacity: 0.5,
+    backgroundColor: '#bfbfbf',
+  },
+  '& .MuiSlider-mark': {
+    backgroundColor: '#bfbfbf',
+    height: 8,
+    width: 1,
+    '&.MuiSlider-markActive': {
+      opacity: 1,
+      backgroundColor: 'currentColor',
+    },
+  },
+}));
+
+export const SliderSkills = ({ areaSkill, level }) => {
+  return (
+    <Box sx={{ width: 320 }}>
+      <Typography gutterBottom variant="subtitle1" sx={{ fontWeight: 700 }}>{ areaSkill }</Typography>
+      <IOSSlider
+        // disableSwap={true}
+        disabled={true}
+        defaultValue={level}
+        aria-label="ios slider"
+        marks={marks}
+        valueLabelDisplay="off"
+        color="secondary"
+      />
+      <Box sx={{ m: 3 }} />
+    </Box>
+  );
+}
--- src/portafolio/components/technology-skills/TechnologySkills.jsx
+++ src/portafolio/components/technology-skills/TechnologySkills.jsx
@@ -1,8 +1,6 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles/';
-import { cardsSkillsData } from '../../../data';
-import { generateId } from '../../../helpers';
-import { Cards } from "./Cards";
+import { SliderSkills } from './SliderSkills';
 
 const useStyles = makeStyles({
   cardsContainer: {
@@ -24,17 +22,49 @@ export const TechnologySkills = () => {
       }}
     >
       <Container maxWidth="lg">
-        <Typography
-          gutterBottom
-          variant="h3"
-          align="center"
-          className="marginHeaders"
-        >
-          Technology Skills
-        </Typography>
+        <Grid container>
+          <Grid item xs={12} justifyContent="center">
+            <Typography
+              gutterBottom
+              variant="h3"
+              align="center"
+              className="marginHeaders"
+            >
+              Technology Skills
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="body1" paragraph align="left">
+              I started as a webmaster, my main interest was a design UI interfaces and graphs, but the way or fate, got me here develop UI interfaces and doing every time of things related with web, that is a very huge and interesant. I’ve done remote work for colleges,  and collaborated with very talented people to create web products for both business and consumer use.
+            </Typography>
+
+            <Typography variant="body1" paragraph align="left">
+              I create successful websites and webapps, always with colleges and coworkers. Always improve my work and upgrade as a person, my focuses is built software with best practices and performance, resposive and usefull. The main area of my expertise is front-end development, but also I have work as a Fullstack developer with CMS wordpress, brighspot and customs systems.
+            </Typography>
+
+            <Typography variant="body1" paragraph align="left">
+              Actually my interest is reforce my design skills in CSS and improve my comunication skills. 
+            </Typography>
+
+            <Typography variant="body1" paragraph align="left">
+              Visit me in my Linkding profile or get in touch!
+            </Typography>
+          </Grid>
+          
+        
+          <Grid item xs={12} md={6} justifyContent="flex-end"
+            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+          >
+            <SliderSkills areaSkill="Front-end" level={70}/>
+            <SliderSkills areaSkill="React" level={55}/>
+            <SliderSkills areaSkill="Back-end" level={40} />
+            <SliderSkills areaSkill="Typescript" level={35}/>
+          </Grid> 
+        </Grid>
+
         <Box className={classes.cardsContainer}>
           {
-            cardsSkillsData.map((data) => <Cards key={generateId()} { ...data }/>)
+            // cardsSkillsData.map((data) => <Cards key={generateId()} { ...data }/>)
           }
         </Box>
       </Container>
